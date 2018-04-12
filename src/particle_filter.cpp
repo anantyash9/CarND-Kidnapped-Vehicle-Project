6399d1e9d8f83628dc4c18e55d1f8e57fcf69908
Anant Yash Pande <anantyash9@gmail.com>
2018-04-13 04:06:46 +0530

    change

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -172,27 +172,14 @@ void ParticleFilter::resample() {
 	vector<Particle> resampled;
 
 	//Random device generator using std::discrete_distribution
-	std::random_device rd;
-	std::mt19937 gen(rd());
-
-	discrete_distribution<int> random_number(0 , num_particles-1);
-	discrete_distribution<int> disct_dis(weights.begin(), weights.end());
-
-	unsigned index = random_number(rd);
-
-	double max_weight = *max_element(weights.begin(), weights.end());
-
-	//resampling
-	double beta =0.0;
-	for (int i =0; i <num_particles; i++)
+	random_device rd;
+	default_random_engine gen(rd());
+	vector<Particle> resampled;
+	resampled.resize(num_particles);
+	for (int i=0;i<num_particles;i++)
 	{
-		beta+=disct_dis(gen) * 2.0 * max_weight;
-		while(beta > weights[index])
-		{
-			beta -= weights[index];
-			index = (index + 1) % num_particles;
-		}
-		resampled.push_back(particles[index]);
+		discrete_distribution<int> r_weight(weights.begin(), weights.end());
+		resampled[i] = particles[r_weight(gen)];
 	}
 	particles = resampled;
 }
