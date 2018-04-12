0fb681bf1a1114d3e20ff4949c69755d728f4f47
Anant Yash Pande <anantyash9@gmail.com>
2018-04-13 01:17:11 +0530

    change

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -151,18 +151,23 @@ void ParticleFilter::resample() {
 	// TODO: Resample particles with replacement with probability proportional to their weight.
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
-	cout<<"resample started"<<endl;
-	random_device rd;
-	default_random_engine gen(rd());
-	vector<Particle> resampled;
-	resampled.resize(num_particles);
-	for (int i=0;i<num_particles;i++)
-	{
-		discrete_distribution<int> r_weight(weights.begin(), weights.end());
-		resampled[i] = particles[r_weight(gen)];
-	}
-particles = resampled;
-cout<<"resample done"<<endl;
+	// Resamples particles with replacement with probability proportional to their weight.
+  
+  // Vector for new particles
+  vector<Particle> new_particles (num_particles);
+  
+  // Use discrete distribution to return particles by weight
+  random_device rd;
+  default_random_engine gen(rd());
+  for (int i = 0; i < num_particles; ++i) {
+    discrete_distribution<int> index(weights.begin(), weights.end());
+    new_particles[i] = particles[index(gen)];
+    
+  }
+  
+  // Replace old particles with the resampled particles
+  particles = new_particles;
+
 }
 
 Particle ParticleFilter::SetAssociations(Particle& particle, const std::vector<int>& associations, 
